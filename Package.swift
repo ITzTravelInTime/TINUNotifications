25299f813064886a5d6d0b7e10a50d9513abf06b
ITzTravelInTime <pietrocaruso97@yahoo.it>
2021-06-27 05:40:29 +0200

    Updated dependancy usage

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -17,7 +17,7 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/ITzTravelInTime/TINURecovery", .branch("main"))
+        .package(url: "https://github.com/ITzTravelInTime/TINURecovery", from: "1.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -25,7 +25,7 @@ let package = Package(
         .target(
             name: "TINUNotifications",
             dependencies: [
-                .product(name: "TINURecovery")
+                .byName(name: "TINURecovery")
             ]),
         .testTarget(
             name: "TINUNotificationsTests",
